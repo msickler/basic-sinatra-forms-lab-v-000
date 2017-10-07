37f0a973ecae40b7cfa7ff6edfb1f0bca0c1e052
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 03:24:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ class App < Sinatra::Base
     @coach = params[:coach]
     @pg = params[:pg]
     @sg = params[:sq]
-    @sf = params[:sf] 
-    @pf = params[:pf] 
+    @sf = params[:sf]
+    @pf = params[:pf]
     @c = params[:c]
-    
+
     erb :team
   end
 
